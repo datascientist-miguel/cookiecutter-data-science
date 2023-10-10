5623a22cc605c821e484200ac6966a77113f413c
Miguel Carvajal <m.angel9106@gmail.com>
2023-10-10 10:13:14 -0300

    change scripts

--- {{ cookiecutter.project_slug }}/scripts/manipulation/sampling_data.py
+++ {{ cookiecutter.project_slug }}/scripts/manipulation/sampling_data.py
@@ -1,7 +1,7 @@
-from sklearn.model_selection import StratifiedShuffleSplit
 import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
 
-def sampling_data(data, sampling_type='random', sampling_fraction=0.1, random_state=42, n_splits=1, stratify_by= None):
+def sampling_data(data, sampling_type='random', sampling_fraction=0.1, random_state=42, n_splits=1, stratify_by=None):
     """
     This function is used to sample data from the dataset
     :param data: input dataset
@@ -22,19 +22,20 @@ def sampling_data(data, sampling_type='random', sampling_fraction=0.1, random_st
         if not 0 < sampling_fraction <= 1.0:
             raise ValueError("Sampling fraction should be between 0 and 1.")
 
-        if not isinstance(n_splits, int) or n_splits < 1:
-            raise ValueError("Number of splits should be a positive integer.")
-        if not isinstance(stratify_by, str) or stratify_by not in data.columns:
-            raise ValueError("Invalid column name for stratified sampling.")
+        if sampling_type == 'stratified':
+            if not isinstance(n_splits, int) or n_splits < 1:
+                raise ValueError("Number of splits should be a positive integer.")
 
-        sampling_stratified = 1.0 - sampling_fraction
+            if not isinstance(stratify_by, str) or stratify_by not in data.columns:
+                raise ValueError("Invalid column name for stratified sampling.")
 
-        if sampling_type == 'random':
-            data_sampling = data.sample(frac=sampling_fraction, random_state=random_state)
-        elif sampling_type == 'stratified':
+            sampling_stratified = 1.0 - sampling_fraction
             sss = StratifiedShuffleSplit(n_splits=n_splits, test_size=sampling_stratified, random_state=random_state)
             for train_index, _ in sss.split(data, data[stratify_by]):
                 data_sampling = data.iloc[train_index]
+        else:
+            data_sampling = data.sample(frac=sampling_fraction, random_state=random_state)
+
         return data_sampling
 
     except ValueError as ve:
