1a03868e76aba6abe3d547b49d539cfd0a2ee9ef
Miguel Carvajal <m.angel9106@gmail.com>
2023-10-10 09:36:46 -0300

    modify some scripts

--- {{ cookiecutter.project_slug }}/scripts/visualization/boxplot.py
+++ {{ cookiecutter.project_slug }}/scripts/visualization/boxplot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
+import seaborn as sns
 def boxplot(data, hue):
     """
     This function is used to plot boxplots for numerical variables
